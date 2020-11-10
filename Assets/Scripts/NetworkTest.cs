9911de2b981a7ceabf369ef0f1377c414bf79e4d
Jonny <jonny.hong91@gmail.com>
2020-11-11 02:00:11 +0800

    导入库

---
+++ Assets/Scripts/NetworkTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon;
+using UnityEngine;
+
+public class NetworkTest : PunBehaviour
+{
+	private void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings("0.0.1");
+	}
+}
